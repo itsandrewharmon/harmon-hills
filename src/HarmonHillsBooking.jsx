79f33f4794b9f70d09103e5701ff2c32b3b091cc
Andrew Harmon <andrewharmon@MacBook-Air.local>
2025-07-23 12:14:28 -0600

    Add professional CSS design

--- src/HarmonHillsBooking.jsx
+++ src/HarmonHillsBooking.jsx
@@ -57,7 +57,7 @@ export default function HarmonHillsBooking() {
   };
 
   return (
-    <div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
+    <><div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
       <h1 style={{ fontSize: '2rem', fontWeight: 'bold', textAlign: 'center' }}>
         Harmon Hills Adventures & Campgrounds
       </h1>
@@ -93,8 +93,7 @@ export default function HarmonHillsBooking() {
         <input
           type="date"
           value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-        />
+          onChange={(e) => setSelectedDate(e.target.value)} />
         <br />
         <button
           onClick={handleBooking}
@@ -104,6 +103,6 @@ export default function HarmonHillsBooking() {
           Confirm Reservation
         </button>
       </div>
-    </div>
+    </div><div class='tableauPlaceholder' id='viz1753294361484' style='position: relative'><noscript><a href='#'><img alt='ICLA Pass Rates by Area – ISU College of Education ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;IC&#47;ICLAPassRatesbyAreaISUCollegeofEducation&#47;ICLAPassRatesbyAreaISUCollegeofEducation&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='ICLAPassRatesbyAreaISUCollegeofEducation&#47;ICLAPassRatesbyAreaISUCollegeofEducation' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;IC&#47;ICLAPassRatesbyAreaISUCollegeofEducation&#47;ICLAPassRatesbyAreaISUCollegeofEducation&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div><script type='text/javascript'>                    var divElement = document.getElementById('viz1753294361484');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) {vizElement.style.width = '1000px'};vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) {vizElement.style.width = '1000px'};vizElement.style.height='827px';} else {vizElement.style.width = '100%'};vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script></>
   );
 }
